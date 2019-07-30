ad8d50be5e75d34463094c0f39bcc4425092978a
Carlton Joseph <carlton.joseph@gmail.com>
2019-07-30 14:38:27 -0700

    add server sockets for async actions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const fs = require("fs");
+const WebSocket = require("ws");
 
 const app = express();
 
@@ -8,27 +9,36 @@ app.use(morgan("combined"));
 app.use(express.static("./build"));
 
 const port = process.env.PORT || 4000;
+const wssport = process.env.WSSPORT || 8080; // web socket server port
 
 app.get("/api/dirs/:path", (req, res) => res.json({ files: ["a", "b"] }));
 app.get("/api/files", getFilesResp);
 app.get("/api/pwd", (req, res) => res.json({ pwd: process.cwd() }));
 app.get("/api/back", (req, res) => changeDir(req, res, "../"));
 app.get("/api/chdir/:dir", (req, res) => changeDir(req, res, req.params.dir));
+app.get("/api/process-images", processImages);
 app.get("/", (req, res) => res.send("Api server. All routes at API!"));
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`));
+const wss = new WebSocket.Server({ port: wssport });
+let wsConnection;
+wss.on("connection", function connection(ws) {
+  console.log(`Web socket server listening on port ${wssport}!`);
+  ws.on("message", m => console.log(`wss message`, m));
+});
+const appState = { image: { processing: false } };
+app.listen(port, () => console.log(`Http server listening on port ${port}!`));
 
-function changeDir(req, res, dir) {
+function changeDir(_, res, dir) {
   if (!fs.existsSync(dir))
     return res.status(410).json({
-      error: `Error! Failed finding ${dir} in ${process.cwd()}.`,
+      error: `Eraaror! Failed finding ${dir} in ${process.cwd()}.`,
       pwd: process.cwd()
     });
   process.chdir(dir);
   res.json({ pwd: process.cwd() });
 }
 
-function getFilesResp(req, res) {
+function getFilesResp(_, res) {
   getFiles(process.cwd())
     .then(files => res.json(files))
     .catch(error => res.json(error));
@@ -54,4 +64,29 @@ function getFiles(path) {
       reject({ error: `Failed, reading ${path} with ${e.message}` });
     }
   });
+}
+
+function wsProgress(status) {
+  wss.clients.forEach(
+    client =>
+      client !== wsConnection &&
+      client.readyState === WebSocket.OPEN &&
+      client.send(JSON.stringify(status))
+  );
+}
+
+function processImages(_, res) {
+  if (appState.image.processing)
+    return res
+      .status(410)
+      .json({ error: "Currently running another process." });
+  appState.image = { processing: true, last: 6, current: 0 };
+  appState.interval = setInterval(() => {
+    if (appState.image.current >= appState.image.last) {
+      appState.image = { processing: false, last: 0, current: 0 };
+      clearInterval(appState.interval);
+    } else appState.image.current++;
+    wsProgress(appState.image);
+  }, 500);
+  return res.json({ status: "Images are being processed." });
 }
--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
 import Card from "@material-ui/core/Card";
 import FolderIcon from "@material-ui/icons/Folder";
+import Button from "@material-ui/core/Button";
+
+const wsUrl = "ws://localhost:8080";
+
+const routes = {
+  files: "/api/files",
+  pwd: "/api/pwd",
+  back: "/api/back",
+  chdir: "/api/chdir",
+  processImages: "/api/process-images"
+};
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,6 +27,10 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3, 2),
     margin: theme.spacing(3, 2)
   },
+  button: {
+    padding: theme.spacing(1),
+    margin: theme.spacing(1)
+  },
   gridC: {
     display: "flex",
     flexWrap: "wrap"
@@ -23,8 +38,8 @@ const useStyles = makeStyles(theme => ({
   card: {
     background: theme.palette.secondary.main,
     color: theme.palette.secondary.contrastText,
-    margin: theme.spacing(),
-    padding: theme.spacing(),
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
     borderRadius: theme.spacing(2),
     "&:hover": {
       background: theme.palette.secondary.dark
@@ -35,13 +50,14 @@ const useStyles = makeStyles(theme => ({
     alignItems: "center"
   },
   icon: {
-    color: theme.palette.primary.light
+    color: theme.palette.primary.light,
+    marginRight: theme.spacing(1)
   },
   error: {
     textAlign: "center",
     background: theme.palette.error.main,
-    margin: theme.spacing(),
-    padding: theme.spacing(),
+    margin: theme.spacing(1),
+    padding: theme.spacing(1),
     borderRadius: theme.spacing(2),
     color: theme.palette.error.contrastText,
     "&:hover": {
@@ -50,21 +66,24 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const routes = {
-  files: "/api/files",
-  pwd: "/api/pwd",
-  back: "/api/back",
-  chdir: "/api/chdir"
-};
-
 function App() {
   const [files, filesSet] = React.useState([]);
   const [pwd, pwdSet] = React.useState("");
   const [err, errSet] = React.useState("");
+  const [imgToProc, imgToProcSet] = React.useState(0);
+  const [imgProced, imgProcedSet] = React.useState(0);
+  const [imgProcing, imgProcingSet] = React.useState(false);
+  const wsConnection = React.useRef(null);
+
   const classes = useStyles();
 
-  const fetchFailed = item => e =>
-    console.error("Fetch failed from", item, "with:", e) || errSet(e);
+  const fetchFailed = item => err => {
+    const e = err.message || err;
+    const msg = `Server fetch failed for ${item} with: ${e}`;
+    console.error(msg);
+    errSet(msg);
+  };
+
   const testPwdE = json => {
     pwdSet(json.pwd);
     if (json.error)
@@ -72,19 +91,32 @@ function App() {
   };
 
   React.useEffect(() => {
-    const { files } = routes;
-    fetch(files)
+    fetch(routes.files)
       .then(data => data.json())
       .then(json => filesSet(json.files))
-      .catch(fetchFailed(files));
+      .catch(fetchFailed(routes.files));
   }, [pwd]);
 
   React.useEffect(() => {
-    const { pwd } = routes;
-    fetch(pwd)
+    fetch(routes.pwd)
       .then(data => data.json())
       .then(testPwdE)
-      .catch(fetchFailed(pwd));
+      .catch(fetchFailed(routes.pwd));
+
+    const ws = new WebSocket(wsUrl);
+    wsConnection.current = ws;
+    ws.onopen = _ => {
+      console.log("connected");
+      ws.send(JSON.stringify({ status: "kick start ws connection" }));
+    };
+    ws.onclose = _ => console.log("disconnected");
+    ws.onmessage = e => {
+      console.log(e);
+      const { current, last, processing } = JSON.parse(e.data);
+      imgProcedSet(current);
+      imgToProcSet(last);
+      imgProcingSet(processing);
+    };
   }, []);
 
   const clicked = file => {
@@ -97,11 +129,21 @@ function App() {
   };
 
   const back = _ => {
-    const { back } = routes;
-    fetch(back)
+    fetch(routes.back)
       .then(data => data.json())
       .then(testPwdE)
-      .catch(fetchFailed(back));
+      .catch(fetchFailed(routes.back));
+  };
+
+  const processImages = _ => {
+    fetch(routes.processImages)
+      .then(data => data.json())
+      .then(data => console.log(data))
+      .catch(fetchFailed(routes.processImages));
+  };
+
+  const preview = _ => {
+    console.log("connection current", wsConnection.current);
   };
 
   return (
@@ -115,24 +157,50 @@ function App() {
       </AppBar>
 
       <Paper className={classes.dirs}>
-        <Typography variant="h6" component="span">
-          Directory:
-        </Typography>{" "}
-        <Typography variant="body2" component="span">
-          {pwd}
-        </Typography>
-        {err && (
-          <Typography
-            variant="body2"
-            component="p"
-            onClick={() => errSet("")}
-            className={classes.error}
+        <Container>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            onClick={processImages}
+            disabled={imgProcing}
           >
-            {err} <b>Click to clear.</b>
+            Process
+          </Button>
+          {imgToProc !== 0 && imgToProc !== imgProced && (
+            <Typography variant="body2" component="span">
+              {imgProced}/{imgToProc}
+            </Typography>
+          )}
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            onClick={preview}
+          >
+            Preview
+          </Button>
+        </Container>
+        <Container>
+          <Typography variant="h6" component="span">
+            Directory:
+          </Typography>{" "}
+          <Typography variant="body2" component="span">
+            {pwd}
           </Typography>
-        )}
+          {err && (
+            <Typography
+              variant="body2"
+              component="p"
+              onClick={() => errSet("")}
+              className={classes.error}
+            >
+              {err} <b>Click to clear.</b>
+            </Typography>
+          )}
+        </Container>
         <Container className={classes.gridC}>
-          {pwd !== "/" ? (
+          {pwd !== "/" && err.length === 0 ? (
             <Card className={classes.card} onClick={back}>
               <Typography variant="body2" component="p">
                 ..
